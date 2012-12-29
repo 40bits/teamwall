556cf4ffadedeb3cf43158b7b9520bf8cf251235
Oliver Wehrens <oliver.wehrens@40bits.com>
2012-12-29 15:22:40 +0100

    added positioning to the configuration, positioning of an instrument and it's configuration are now separated

--- js/teamwall.js
+++ js/teamwall.js
@@ -1,63 +1,76 @@
 function TeamwallApp() {
 
-  var instruments = [];
+    var instruments = [];
 
-  this.loadDashboard = function (dashboardFile) {
+    this.loadDashboard = function (dashboardFile) {
 
-    $.ajax({
-             url:dashboardFile,
-             dataType:'json',
-             cache:false,
-             success:function (data) {
-               jQuery.each(data, function () {
-                 var instrumentData = this;
-                 var instrument;
+        $.ajax({
+            url: dashboardFile,
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                jQuery.each(data.instruments, function createInstruments() {
+                    var instrument;
+                    var instrumentConfig = this;
 
-                 var canvas = document.createElement("canvas");
-                 canvas.id = instrumentData.id;
-                 canvas.width = instrumentData.width;
-                 canvas.height = instrumentData.height;
-                 canvas.setAttribute("style", "margin: 5px");
-                 document.body.appendChild(canvas);
+                    switch (instrumentConfig.instrument) {
+                        case "percent" :
+                            instrument = teamwall.instrument.percent(instrumentConfig);
+                            break;
+                        case "buildchain" :
+                            instrument = teamwall.instrument.buildChain(instrumentConfig);
+                            break;
+                        case "number" :
+                            instrument = teamwall.instrument.number(instrumentConfig);
+                            break;
+                        case "buildalert" :
+                            instrument = teamwall.instrument.buildAlert(instrumentConfig);
+                            break;
+                        default:
+                            break;
+                    }
+                    instruments.push(instrument);
+                });
 
-                 switch (instrumentData.instrument) {
-                   case "percent" :
-                     instrument = teamwall.instrument.percent(instrumentData);
-                     break;
-                   case "buildchain" :
-                     instrument = teamwall.instrument.buildChain(instrumentData);
-                     break;
-                   case "number" :
-                     instrument = teamwall.instrument.number(instrumentData);
-                     break;
-                   case "buildalert" :
-                     instrument = teamwall.instrument.buildAlert(instrumentData);
-                     break;
-                   default:
-                     break;
-                 }
-                 instruments.push(instrument);
-               });
-             },
-             statusCode:{
-               404:function () {
-                 alert("Please add a "+dashboardFile+" file to the installation.");
-               }}
-           });
-    window.setInterval(updateInstruments, 1000);
-  };
+                for (var i = 0; i < instruments.length; i++) {
+                    var instrument = instruments[i];
+                    for (var j = 0; j < data.layouts.length; j++) {
+                        var layout = data.layouts[j];
+                        if (layout.id == instrument.getConfiguration().id) {
+                            var canvas = document.createElement("canvas");
+                            canvas.id = layout.id;
+                            canvas.width = layout.width;
+                            canvas.height = layout.height;
+                            document.body.appendChild(canvas);
+                            $(canvas).css({
+                                position: "absolute",
+                                top: layout.top,
+                                left: layout.left
+                            }).appendTo('body');
+                        }
+                    }
+                }
+            },
+            statusCode: {
+                404: function () {
+                    alert("Please add a " + dashboardFile + " file to the installation.");
+                }
+            }
+        });
+        window.setInterval(updateInstruments, 1000);
+    };
 
-  function updateInstruments() {
-    jQuery.each(instruments, function () {
-      var instrument = this;
-      $.ajax({
-               url:instrument.getConfiguration().url,
-               dataType:'json',
-               cache:false,
-               success:function (data) {
-                 instrument.setValue(data);
-               }
-             });
-    });
-  }
+    function updateInstruments() {
+        jQuery.each(instruments, function () {
+            var instrument = this;
+            $.ajax({
+                url: instrument.getConfiguration().url,
+                dataType: 'json',
+                cache: false,
+                success: function (data) {
+                    instrument.setValue(data);
+                }
+            });
+        });
+    }
 }
