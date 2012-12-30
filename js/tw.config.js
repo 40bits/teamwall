be0f13f3369bc62db4c7f0d2e05fb021e286940a
Oliver Wehrens <oliver.wehrens@40bits.com>
2012-12-30 01:00:08 +0100

    new better matching colors

--- js/tw.config.js
+++ js/tw.config.js
@@ -1,11 +1,11 @@
 teamwall.configuration = {
     colorOk:"#6b9c28",
-    colorBackground:"#3f4040",
-    colorFailure:"red",
-    colorWarning:"#fdd749",
+    colorBackground:"#373636",
+    colorFailure:"#F40E2a",
+    colorWarning:"#e6bd20",
     font:"Doppio One",
     colorText:"#e1dbd8",
-    background:"#585757"
+    background:"#454545"
 };
 
 
