4af4e8b1b09ad50616a663a271969ae707b9a363
Oliver Wehrens <oliver.wehrens@40bits.com>
2013-01-04 22:46:38 +0100

    deleting instruments works, showing the new config as well

--- js/teamwall.js
+++ js/teamwall.js
@@ -90,10 +90,56 @@ function TeamwallApp() {
     };
 
     this.activateConfigUI = function activateConfigUI() {
-        $("#configui").click(function () {
+        $(teamwall.configuration.domIdConfigButton).click(function () {
             teamwall.app.configUiActive = !teamwall.app.configUiActive;
             $("." + teamwall.configuration.cssClassInstrument).draggable("option", "disabled", !teamwall.app.configUiActive);
             jQuery("." + teamwall.configuration.cssClassInstrument).toggleClass(teamwall.configuration.cssClassDraggable);
+            $(teamwall.configuration.cssClassConfigUi).toggle();
+            if (!teamwall.app.configUiActive) {
+                $('.ui-state-disabled').removeClass('ui-state-disabled');
+            }
+        });
+
+        $(teamwall.configuration.cssSelectorTrashCan).droppable({
+            hoverClass: teamwall.configuration.cssClassAboutToBeDeleted,
+            drop: function (event, ui) {
+                deleteInstrument(ui.draggable.attr('id'));
+            }});
+
+        $(teamwall.configuration.cssSelectorSaveDashboard).click(function () {
+            var layouts = [];
+            jQuery.each(teamwall.app.canvases, function () {
+                var canvas = this;
+                var layout = {};
+                layout.id = canvas.id;
+                layout.top = canvas.offsetTop;
+                layout.left = canvas.offsetLeft;
+                layout.width = canvas.width;
+                layout.height = canvas.height;
+                layouts.push(layout);
+            });
+
+            var instrumentConfigurations = [];
+            jQuery.each(teamwall.app.instruments, function () {
+                instrumentConfigurations.push(this.getConfiguration())
+            });
+
+            var teamwallConfiguration = {};
+            teamwallConfiguration.layouts = layouts;
+            teamwallConfiguration.instruments = instrumentConfigurations;
+
+            $(teamwall.configuration.cssSelectorNewDashboardConfigDialog).text(JSON.stringify(teamwallConfiguration));
+
+            $(teamwall.configuration.cssSelectorNewDashboardConfigDialog).dialog({
+                modal: false,
+                minWidth: 600,
+                resizable: false,
+                buttons: {
+                    Close: function () {
+                        $(this).dialog("close");
+                    }
+                }
+            });
         })
     };
 
@@ -124,29 +170,6 @@ function TeamwallApp() {
         function stopDragging() {
             var canvas = this;
             canvas.style.zIndex = 1;
-
-            var layouts = [];
-            jQuery.each(teamwall.app.canvases, function () {
-                var canvas = this;
-                var layout = {};
-                layout.id = canvas.id;
-                layout.top = canvas.offsetTop;
-                layout.left = canvas.offsetLeft;
-                layout.width = canvas.width;
-                layout.height = canvas.height;
-                layouts.push(layout);
-            });
-
-            var instrumentConfigurations = [];
-            jQuery.each(teamwall.app.instruments, function () {
-                instrumentConfigurations.push(this.getConfiguration())
-            });
-
-            var teamwallConfiguration = {};
-            teamwallConfiguration.layouts = layouts;
-            teamwallConfiguration.instruments = instrumentConfigurations;
-
-            console.log(JSON.stringify(teamwallConfiguration));
         }
 
         function startDragging() {
@@ -155,6 +178,44 @@ function TeamwallApp() {
         }
     }
 
+    function deleteInstrument(id) {
+        function deleteFromDom() {
+            jQuery.each(teamwall.app.canvases, function () {
+                if (jQuery(this).attr('id') == id) {
+                    document.body.removeChild(this);
+                }
+            })
+        }
+
+        function deleteFromCanvasArray() {
+            var indexOfElement = -1;
+            for (var count = 0; count < teamwall.app.canvases.length ; count++) {
+                if (jQuery(teamwall.app.canvases[count]).attr('id') == id) {
+                    indexOfElement = count;
+                }
+            }
+            if (indexOfElement > -1 ) {
+                teamwall.app.canvases.splice(indexOfElement, 1);
+            }
+        }
+
+        function deleteFromInstrumentArray() {
+            var indexOfElement = -1;
+            for (var count = 0; count < teamwall.app.instruments.length ; count++) {
+                if (teamwall.app.instruments[count].getConfiguration().id == id) {
+                    indexOfElement = count;
+                }
+            }
+            if (indexOfElement > -1 ) {
+                teamwall.app.instruments.splice(indexOfElement, 1);
+            }
+        }
+
+        deleteFromDom();
+        deleteFromCanvasArray();
+        deleteFromInstrumentArray();
+
+    }
 
     window.setInterval(updateInstruments, teamwall.configuration.instrumentUpdateInterval);
 
--- js/tw.config.js
+++ js/tw.config.js
@@ -1,14 +1,23 @@
 teamwall.configuration = {
-    colorOk:"#6b9c28",
-    colorWarning:"#fdd749",
-    colorBackground:"#373636",
-    colorFailure:"#F40E2a",
-    colorText:"#e1dbd8",
-    font:"Doppio One",
-    instrumentBackground:"#454545",
-    cssClassSelected : "selected",
-    cssClassInstrument : "instrument",
-    cssClassDraggable : "draggable",
+
+    colorOk: "#6b9c28",
+    colorWarning: "#fdd749",
+    colorBackground: "#373636",
+    colorFailure: "#F40E2a",
+    colorText: "#e1dbd8",
+    font: "Doppio One",
+    instrumentBackground: "#454545",
+
+    cssClassSelected: "selected",
+    cssClassInstrument: "tw-instrument",
+    cssClassDraggable: "draggable",
+    domIdConfigButton: "#configbutton",
+    cssClassConfigUi: ".configui",
+    domIdConfigBar: "#configbar",
+    cssSelectorTrashCan: "#configui_trashCan",
+    cssSelectorSaveDashboard: "#configui_saveDashboard",
+    cssClassAboutToBeDeleted: "instrumentHoverTrash",
+    cssSelectorNewDashboardConfigDialog: "#dialog-newdashboardconfig",
     instrumentUpdateInterval: 1000
 };
 
