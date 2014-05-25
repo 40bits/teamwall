de502f107b6a1e0e0e30076cea9d79c55e67ef13
Oliver Wehrens <oliver@wehrens.de>
2014-05-25 12:16:06 +0200

    support multiple lines in one graph

--- js/instrument/tw.instrument.linechart.js
+++ js/instrument/tw.instrument.linechart.js
@@ -1,8 +1,12 @@
 teamwall.instrument.lineChart = function (configuration) {
 
+
     function LineChartInstrument(configuration) {
 
         var instrumentConfiguration = configuration;
+        var fontYAxisHeight, fontXAxisHeight = undefined;
+
+
 
         this.setValue = function (data) {
             drawInstrument(data);
@@ -16,23 +20,10 @@ teamwall.instrument.lineChart = function (configuration) {
             return "canvas";
         }
 
-        function drawInstrument(data) {
-            var canvas = document.getElementById(instrumentConfiguration.id);
-            var context = canvas.getContext("2d");
-            var centerX = canvas.width / 2;
-            var centerY = canvas.height / 2;
-            var highestValuesInGraph, lowestValueInGraph = undefined;
 
-
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            context.fillStyle = teamwall.configuration.instrumentBackground;
-            context.fillRect(0, 0, canvas.width, canvas.height);
-            teamwall.render.writeText(context, instrumentConfiguration.title, centerX, teamwall.render.yPointForDrawingHeading(canvas), teamwall.render.fontForHeader(canvas), teamwall.configuration.colorText);
-
+        function drawAxis(context, canvas) {
             context.beginPath();
 
-            fontYAxisHeight = 50;
-            fontXAxisHeight = 50;
 
             var spaceForHeading = canvas.height * 0.1;
             context.moveTo(fontXAxisHeight, spaceForHeading);
@@ -40,28 +31,47 @@ teamwall.instrument.lineChart = function (configuration) {
             context.lineTo(canvas.width - spaceForHeading, canvas.height - fontYAxisHeight);
 
             context.lineCap = "round";
-            context.lineWidth = 1;
+            context.lineWidth = 3;
             context.strokeStyle = "white";
             context.fillStyle = "white";
             context.stroke();
+        }
 
-            console.log("Number of values: " + data.values.length)
 
-            data.values.forEach(function (entry) {
-                if (entry > highestValuesInGraph || highestValuesInGraph == undefined) {
-                    highestValuesInGraph = entry;
-                }
-                if (entry < lowestValueInGraph || lowestValueInGraph == undefined) {
-                    lowestValueInGraph = entry;
-                }
+        function drawInstrument(data) {
+            var canvas = document.getElementById(instrumentConfiguration.id);
+            var context = canvas.getContext("2d");
+            var centerX = canvas.width / 2;
+            var centerY = canvas.height / 2;
+            var highestValuesInGraph, lowestValueInGraph = undefined;
+            fontYAxisHeight= 50;
+            fontXAxisHeight = 50;
+
+
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.fillStyle = teamwall.configuration.instrumentBackground;
+            context.fillRect(0, 0, canvas.width, canvas.height);
+            teamwall.render.writeText(context, instrumentConfiguration.title, centerX, teamwall.render.yPointForDrawingHeading(canvas), teamwall.render.fontForHeader(canvas), teamwall.configuration.colorText);
+            console.log("Number of values: " + data.labels.length)
+
+            data.charts.forEach(function (chart) {
+                chart.values.forEach(function (entry) {
+                    if (entry > highestValuesInGraph || highestValuesInGraph == undefined) {
+                        highestValuesInGraph = entry;
+                    }
+                    if (entry < lowestValueInGraph || lowestValueInGraph == undefined) {
+                        lowestValueInGraph = entry;
+                    }
+                });
+
             });
 
             var lengthYAxis = canvas.height - 10 - fontYAxisHeight;
             var lengthXAxis = canvas.width - 10 - fontXAxisHeight;
             var ySteps = Math.floor(lengthYAxis / highestValuesInGraph);
-            var xSteps = Math.floor(lengthXAxis / data.values.length);
+            var xSteps = Math.floor(lengthXAxis / data.labels.length);
             var virtualYZero = canvas.height - fontYAxisHeight;
-            ;
             var virtualXZero = fontYAxisHeight;
 
             console.log("Highest Value is " + highestValuesInGraph);
@@ -74,24 +84,25 @@ teamwall.instrument.lineChart = function (configuration) {
             console.log("Virtual Y Zero is at " + virtualYZero)
 
 
-            context.beginPath();
-            context.moveTo(virtualXZero, virtualYZero);
-            var counter = 0;
-            data.values.forEach(function (entry) {
-                var yPoint = virtualYZero - (entry * ySteps);
-                var xPoint = virtualXZero + (counter * xSteps);
-                context.lineTo(xPoint, yPoint);
-                counter++;
-                console.log("Line to X: " + xPoint + "Y:" + yPoint+" Value "+entry);
-
+            data.charts.forEach(function (chart) {
+                var counter = 0;
+                context.beginPath();
+                context.moveTo(virtualXZero, virtualYZero);
+                chart.values.forEach(function (entry) {
+                    var yPoint = virtualYZero - (entry * ySteps);
+                    var xPoint = virtualXZero + (counter * xSteps);
+                    context.lineTo(xPoint, yPoint);
+                    counter++;
+                    console.log("Line to X: " + xPoint + "Y:" + yPoint + " Value " + entry);
+                });
+                context.lineCap = "round";
+                context.lineWidth = 1;
+                context.strokeStyle = chart.color;
+                context.stroke();
 
             });
 
-            context.lineCap = "round";
-            context.lineWidth = 1;
-            context.strokeStyle = "white";
-            context.fillStyle = "white";
-            context.stroke();
+            drawAxis(context, canvas);
 
 
         }
